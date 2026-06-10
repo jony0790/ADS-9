46648bdfaaf7bbc8bb29f983db662f2ed7c695cc
Джалоладдин <joniatabekovich07@gmail.com>
2026-06-10 04:50:44 +0300

    Джумабаев Джалоладдин 25-ИСТ-1-1

--- include/tree.h
+++ include/tree.h
@@ -2,4 +2,31 @@
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 
+#include <vector>
+
+struct Node {
+  char value;
+  std::vector<Node*> children;
+
+  explicit Node(char v = '\0') : value(v) {}
+};
+
+class PMTree {
+ public:
+  Node* root;
+
+  explicit PMTree(const std::vector<char>& data);
+  ~PMTree();
+
+ private:
+  void build(Node* node, std::vector<char> elems);
+  void clear(Node* node);
+};
+
+std::vector<std::vector<char>> getAllPerms(PMTree& tree);
+
+std::vector<char> getPerm1(PMTree& tree, int num);
+
+std::vector<char> getPerm2(PMTree& tree, int num);
+
 #endif  // INCLUDE_TREE_H_
